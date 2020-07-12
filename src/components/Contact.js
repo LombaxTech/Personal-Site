1b5a78561c17e867d34fe82d00f7fd5b8eb92ed2
Rakib Khan <rakibkhan@live.co.uk>
2020-07-13 00:31:02 +0100

    .

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import "../styles/contact.scss";
 
 export default function Contact() {
-    return <div>contact</div>;
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <div style={{ position: "relative", zIndex: 1 }}>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
+
+            <Modal
+                className="modal"
+                show={show}
+                onHide={handleClose}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton className="modal-header">
+                    {/* <h2>My Title</h2> */}
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Woohoo, you're reading this text in a modal!
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
