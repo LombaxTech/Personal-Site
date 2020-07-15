ef00d7ade93207763014f4bea1db38b4b0df614c
Rakib Khan <rakibkhan@live.co.uk>
2020-07-16 00:07:16 +0100

    fixed outside nav click using new npm package

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/mobilenav.scss";
 import { Link } from "react-router-dom";
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function MobileNav() {
     const [show, setShow] = useState(false);
@@ -11,23 +12,29 @@ export default function MobileNav() {
 
     return (
         <div className="mobile-nav">
-            <div className="toggle-nav" onClick={toggleMenu}>
-                MENU
-            </div>
-            <nav style={{ display: show ? "block" : "none" }}>
-                <Link className="mobile-nav-link" to="/">
-                    Home
-                </Link>
-                <Link className="mobile-nav-link" to="/projects">
-                    Projects
-                </Link>
-                <Link className="mobile-nav-link" to="/about">
-                    About
-                </Link>
-                <Link className="mobile-nav-link" to="/contact">
-                    Contact
-                </Link>
-            </nav>
+            <OutsideClickHandler
+                onOutsideClick={() => {
+                    setShow(false);
+                }}
+            >
+                <div className="toggle-nav" onClick={toggleMenu}>
+                    MENU
+                </div>
+                <nav style={{ display: show ? "block" : "none" }}>
+                    <Link className="mobile-nav-link" to="/">
+                        Home
+                    </Link>
+                    <Link className="mobile-nav-link" to="/projects">
+                        Projects
+                    </Link>
+                    <Link className="mobile-nav-link" to="/about">
+                        About
+                    </Link>
+                    <Link className="mobile-nav-link" to="/contact">
+                        Contact
+                    </Link>
+                </nav>
+            </OutsideClickHandler>
         </div>
     );
 }
